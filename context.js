064020d86ba1f1348493b55142916af946f73304
huntj0n <hunt.jon19@gmail.com>
2020-07-24 08:54:36 -0600

    constructors number 7 worked in repl but not in jasmine

--- context.js
+++ context.js
@@ -18,6 +18,14 @@
 */
 
 //Code Here
+const user = {
+  username: "hal",
+  email:"hal@hal.com",
+  getUsername: function(){
+    return this.username
+  }
+}
+user.getUsername()
 
 ////////// PROBLEM 2 //////////
 
@@ -30,7 +38,7 @@ function Animal(name, species, food) {
   this.species = species
   this.food = food
 
-  this.eat = function() {
+  this.eat = function() { 
     return (
       this.name + ' is a ' + this.species + ' and likes to eat ' + this.food
     )
@@ -38,6 +46,8 @@ function Animal(name, species, food) {
 }
 
 //Code Here
+const animal1 = new Animal("fitz", "dog", "bones")
+animal1.eat()
 
 ////////// PROBLEM 3 //////////
 
@@ -56,6 +66,7 @@ let who = {
 }
 
 //Code Here
+const whoSaysHi = sayHi.bind(who)
 
 ////////// PROBLEM 4 //////////
 
@@ -68,7 +79,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = whatIsThis()
 
 let product = {
   name: 'snake plant',
@@ -80,7 +91,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -91,7 +102,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 function Family(numParents, numKids, numPets) {
   this.numParents = numParents
@@ -106,4 +117,4 @@ function Family(numParents, numKids, numPets) {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-// let context4 = ???
+let context4 = family1
